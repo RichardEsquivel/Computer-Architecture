86456a1569e6584783924c211396706aa8f0cc27
RichardEsquivel <rlesquivel@gmail.com>
2020-04-23 19:44:36 -0500

    Call and Return implemented and call.ls8 working, MVP complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,7 +3,6 @@
 import sys
 
 
-
 class CPU:
     """Main CPU class."""
 
@@ -30,20 +29,26 @@ class CPU:
         # Register 7        assigned to STARTING memory[index] 244(0xF4) for STACK processes PUSH/POP
         self.reg[self.sp] = self.ram[self.sp_mem_index]
 
-        # Program Machine Codes
+        # Program Machine Codes  #this could be improved.  Probably not ideal to have the Machine codes.
         self.OP_LDI = 0b10000010
         self.OP_PUSH = 0b01000101
         self.OP_POP = 0b01000110
+        self.OP_CALL = 0b01010000
+        self.OP_RET = 0b00010001
         self.OP_PRN = 0b01000111
+        self.OP_ADD = 0b10100000
         self.OP_MUL = 0b10100010
         self.OP_HLT = 0b00000001
 
-        # Dispatch Table - Beautifying RUN:
+        # Dispatch Table - Beautifying RUN  # likely a better way to dynamically do this.
         self.dispatchtable = {}
         self.dispatchtable[self.OP_LDI] = self.handle_LDI
         self.dispatchtable[self.OP_PUSH] = self.handle_PUSH
         self.dispatchtable[self.OP_POP] = self.handle_POP
+        self.dispatchtable[self.OP_CALL] = self.handle_CALL
+        self.dispatchtable[self.OP_RET] = self.handle_RET
         self.dispatchtable[self.OP_PRN] = self.handle_PRN
+        self.dispatchtable[self.OP_ADD] = self.handle_ADD
         self.dispatchtable[self.OP_MUL] = self.handle_MUL
         self.dispatchtable[self.OP_HLT] = self.handle_HLT
 
@@ -86,22 +91,7 @@ class CPU:
                 # print(type(int(line, base=2)))
                 self.address += 1
 
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
+    # The computer's ALU is responsible for processing mathematical calculations.
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -144,19 +134,6 @@ class CPU:
         self.reg[opa] = opb
         self.pc += increment
 
-    # register
-    [
-        # pop direction
-        # assign register[0-2] to be equal to memory[memoryindexinhex]
-    ]
-
-    # Memory
-    [
-        # push direction
-        # assign memory[memoryindexinhex] to be = to something in register[0-2]
-        # starting at memory[244]
-    ]
-
     def handle_PUSH(self, increment, opa):
         self.sp_mem_index -= 1
         self.ram[self.sp_mem_index] = self.reg[opa]
@@ -169,6 +146,23 @@ class CPU:
         self.sp_mem_index += 1
         self.pc += increment
 
+    def handle_CALL(self, increment, opa):
+        next_instruct = (self.pc + increment)
+
+        # decrement memory stack location, add next instruct to stack
+        self.sp_mem_index -= 1
+        self.ram[self.sp_mem_index] = next_instruct
+        # print(self.ram[self.sp_mem_index], "EHFIELF")
+        # reassign self.pc to value in given register
+        self.pc = self.reg[opa]
+
+    def handle_RET(self):
+        # pop return address from top of stack
+        ret_address = self.ram[self.sp_mem_index]
+        self.sp_mem_index += 1
+
+        self.pc = ret_address
+
     def handle_PRN(self, increment, opa):
         # print(f"Register[{opa}]!!!: ", hex(self.reg[opa]).lstrip("0x"))
         print(f"Register[{opa}]!!!: ", self.reg[opa])
@@ -176,6 +170,10 @@ class CPU:
 
     def handle_MUL(self, increment, opa, opb):
         self.alu("MUL", opa, opb)
+        self.pc += increment
+
+    def handle_ADD(self, increment, opa, opb):
+        self.alu("ADD", opa, opb)
         self.pc += increment
 
     def handle_HLT(self):
@@ -191,8 +189,7 @@ class CPU:
             self.ir = self.ram_read(self.pc)  # address 0
             operand_a = self.ram_read(self.pc + 1)  # address 1   # R0
             operand_b = self.ram_read(self.pc + 2)  # address 2   # 8
-
-            # track the instruction length to increment self.pc dynamically.
+            # Track the instruction length to increment self.pc dynamically.
             # 1. `AND` the Instruction against binary isolator
             #   Binary Isolator uses a 1 in the location of what you want to keep
             # i.e. if instruction or self.ir in this case is 01000111, the 01 at the beginning of the binary value tells us how many arguments and operand values follow in the instruction file(see .ls8 file). So we would use 11000000 then do (01000111 & 11000000) to get the result 0f 01000000 then do step 2
@@ -200,31 +197,13 @@ class CPU:
             # 3. Increment 1 to move to the NEXT instruction
             len_instruct = ((self.ir & 11000000) >> 6) + 1
 
-            # LDI
-            if self.ir == self.OP_LDI:
-                self.dispatchtable[self.OP_LDI](
-                    len_instruct, operand_a, operand_b)
-
-            # PUSH
-            elif self.ir == self.OP_PUSH:
-                self.dispatchtable[self.OP_PUSH](len_instruct, operand_a)
-
-            # POP
-            elif self.ir == self.OP_POP:
-                self.dispatchtable[self.OP_POP](len_instruct, operand_a)
-
-            # PRN
-            elif self.ir == self.OP_PRN:
-                self.dispatchtable[self.OP_PRN](len_instruct, operand_a)
-
-            # MUL
-            elif self.ir == self.OP_MUL:
-                self.dispatchtable[self.OP_MUL](
-                    len_instruct, operand_a, operand_b)
-
-            # HLT
-            elif self.ir == self.OP_HLT:
-                self.dispatchtable[self.OP_HLT]()
-
+            # Branchtable/Dispatchtable example version...?  Not working as expected.
+            if len_instruct == 3:
+                self.dispatchtable[self.ir](len_instruct, operand_a, operand_b)
+            elif len_instruct == 2:
+                self.dispatchtable[self.ir](len_instruct, operand_a)
+            elif len_instruct == 1:
+                self.dispatchtable[self.ir]()
             else:
                 print("Unknown Instruction")
+
